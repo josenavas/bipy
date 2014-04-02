ff51baf74b8195ae1268b4bc697c5fec5694d209
Jose Navas <josenavasmolina@gmail.com>
2014-04-02 10:34:23 -0600

    Adding more tests

--- skbio/maths/unifrac/fast_unifrac.py
+++ skbio/maths/unifrac/fast_unifrac.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Copyright (c) 2013--, bipy development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file COPYING.txt, distributed with this software.
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 """Fast implementation of UniFrac for use with very large datasets"""
 from __future__ import division
